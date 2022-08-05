a357d82a59369e788e05cf0cc6ba9701d5148c05
natancolen <natancolen@gmail.com>
2022-08-04 23:58:17 -0300

    melhorando a vizualizacao #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NovaTransferenciaComponent } from './nova-transferencia/nova.transferencia.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+
+import localePT from '@angular/common/locales/pt';
+
+registerLocaleData(localePT, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransferenciaComponent,
-    ExtratoComponent
+  declarations: [ AppComponent, NovaTransferenciaComponent, ExtratoComponent ],
+  imports: [ BrowserModule, FormsModule ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
